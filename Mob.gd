f9d8e358a5a1b4664bb54a0a853abf68f4a2e821
WINDA SEVYLLIA <winda.2020207209264@student.umpri.ac.id>
2024-06-05 13:24:32 +0700

    Update Mob.gd

--- Mob.gd
+++ Mob.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 func _ready():
 	$AnimatedSprite2D.play()
 	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
+	
 	$AnimatedSprite2D.animation = mob_types.pick_random()
 
 
